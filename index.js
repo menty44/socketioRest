20a00dfecbdbb3112ed659df3feec944c6ee484d
menty44 <menty44@gmail.com>
2020-05-22 09:22:26 +0300

    fixed socket to web

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
+
 const app = express()
-const Swal = require('sweetalert2')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(express.static('public'))
 var server = app.listen(3000);
 
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+app.set('socketio', io);
 var EventEmitter = require('events')
 
 var superman = '';
 var ee = new EventEmitter()
 
 //we define the variables
-var sendResponse = function () {};
+var sendResponse = function () {
+};
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected X');
 
     ee.on('message', function (text) {
         console.log('EventEmitter ', text)
         superman = text;
         let foo = superman
-        for(var i in foo){
+        for (var i in foo) {
             //alert(i); // alerts key
             //alert(foo[i]); //alerts key's value
         }
-        socket.emit('chat message', foo[i])
+        socket.emit('chat message', text)
     })
 
-
-    socket.on('chat message', function(text){
-        console.log('send the message to everyone, including the sender. HHHH : ' + superman);
-        io.emit('chat message', superman);
+    io.on('connection',function(socket) {
+        console.log('made socket connection');
+        socket.on('chat', function(data){
+            // io.sockets.emit('chat',data);
+            console.log(data);
+        });
     });
 
-    socket.on('taskResponse', data => {
-        //calling a function which is inside the router so we can send a res back
-        sendResponse(data);
-    })
+    // socket.on('connection', function (client) {
+    //     client.on('chat message', this.onSocketMessage);
+    //     // client.on('disconnect', this.onSocketDisconnect);
+    // }.bind(this));
 
-    socket.on('connection', function(client) {
-        client.on('chat message', this.onSocketMessage);
-        client.on('disconnect', this.onSocketDisconnect);
-    }.bind(this));
 
-    socket.on('chat message', function(d){
-        console.log('message: ' + d);
+    // socket.on('chat message', function (d) {
+    //     console.log('send the message to everyone, including the sender. : ' + d);
+    //     io.emit('chat message', d);
+    // });
 
-      });
-      socket.on('chat message', function(d){
-        console.log('send the message to everyone, including the sender. : ' + d);
-        io.emit('chat message', d);
-      });
+});
 
-  });
 
-app.set('socketio', io);
 
-app.post('/', function (req, res, next) {
-
+app.post('/', async function (req, res) {
+    console.log('request', req.body.latitude);
+    console.log('request', req.body.longitude);
     var io = req.app.get('socketio');
 
-    io.to('chat message').emit('chat message', req.query);
-  res.send('Hello World EventEmitter')
-  console.log('request', req.query);
-  ee.emit('message', req.query)
-})
+    io.to('message').emit('chat message', JSON.stringify(req.body.latitude));
+    //res.send('Hello World EventEmitter')
+    ee.emit('message', req.body.latitude+', '+req.body.latitude);
+    res.json(req.body)
+});
 
-app.get('/sock', function(req, res){
-    res.sendFile(__dirname +'/sockets.html');
-  });
+app.get('/sock', function (req, res) {
+    res.sendFile(__dirname + '/sockets.html');
+});
